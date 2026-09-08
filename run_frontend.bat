d65916c5ef2470a7eba44f69a745c3bae9f2d852
Abhishek Anand <abhishekanand4245@gmail.com>
2026-09-09 00:01:20 +0530

    Remove run_frontend.bat and run_backend.bat

--- run_frontend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ========================================================
-echo   Starting PulseOps React Frontend (Vite)
-echo ========================================================
-cd frontend
-npm run dev
-pause
